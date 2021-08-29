f143e809168c23ec0f7ab71334176012f8e36e6d
beetseeds <89722113+beetseeds@users.noreply.github.com>
2021-08-29 14:45:38 +0800

    dev

---
+++ delete_oldbuild.bat
@@ -0,0 +1,14 @@
+rmdir venv /s /q
+
+rmdir build_scripts\build /s /q
+rmdir build_scripts\dist /s /q
+rmdir build_scripts\win_build /s /q
+
+rmdir beet-blockchain-gui\build /s /q
+rmdir beet-blockchain-gui\daemon /s /q
+rmdir beet-blockchain-gui\node_modules /s /q
+
+rmdir beet-blockchain-gui\beet-win32-x64 /s /q
+rmdir beet-blockchain-gui\release-builds /s /q
+
+
